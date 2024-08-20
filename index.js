ae63e66f36e4fa89ceb5ef02c892896ae496276e
RicardoRam781 <alexrp782@gmail.com>
2024-08-19 21:06:34 -0600

    change index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const router = require("./src/router/router");
+const logger = require('morgan');
+const cors = require("cors")
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(logger('dev'));
+app.use(cors())
+app.use(express.json())
+app.use("",router);
+
+app.listen(port, () => {
+    console.log(`Listeting in http://localhost:${port}`)
+})

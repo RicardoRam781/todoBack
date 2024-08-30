a278520c48757b79e6c19812c8e90ecc90fc82ab
RicardoRam781 <alexrp782@gmail.com>
2024-08-30 13:43:50 -0600

    add blocking

--- src/controllers /ToDoController.js
+++ src/controllers /ToDoController.js
@@ -2,19 +2,19 @@
 
 const toDoService = require("../services/toDoServices")
 
-function getToken(req){
+function getToken(req) {
     return req.headers.authorization?.split('Bearer ')[1];
 }
 
 const getAll = async (req, res) => {
     const token = getToken(req)
-    if(!token){
+    if (!token) {
         res.status(401).json([])
-    } else{
+    } else {
         const todoList = await toDoService.getAll(token)
         res.json((todoList))
     }
-    
+
 }
 const getTodo = async (req, res) => {
     const todo = await toDoService.getTodo(req.params.id)
@@ -22,27 +22,27 @@ const getTodo = async (req, res) => {
 }
 const addTodo = async (req, res) => {
     const token = getToken(req)
-    if(!token){
+    if (!token) {
         res.status(401).json("You have to log in")
     }
-    
-        
-        const todo = toDoService.addTodo(req.body, token)
-        if (todo) {
-            res.status(201).json("Created")
-    
-        } else {
-            res.status(500).json("Internal error")
-        }
+
+
+    const todo = toDoService.addTodo(req.body, token)
+    if (todo) {
+        res.status(201).json("Created")
+
+    } else {
+        res.status(500).json("Internal error")
+    }
     console.log("todo", todo)
-    
+
 }
 const updateTodo = async (req, res) => {
     const token = getToken(req)
-    if(!token){
+    if (!token) {
         res.status(401).json('Not autorized')
     }
-    const todoUpdate = await toDoService.updateTodo(req.body, req.params.id,token)
+    const todoUpdate = await toDoService.updateTodo(req.body, req.params.id, token)
     if (todoUpdate) {
         res.status(204).send("updated")
     } else {
@@ -51,29 +51,41 @@ const updateTodo = async (req, res) => {
 }
 const deleteTodo = (req, res) => {
     const token = getToken(req)
-    if(!token){
+    if (!token) {
         res.status(401).json('Not autorized')
     }
-    const todoDelete = toDoService.deleteTodo(req.params.id,token)
+    const todoDelete = toDoService.deleteTodo(req.params.id, token)
     if (todoDelete) {
         res.status(204).json("deleted")
     } else {
         res.status(500).json("Internal error")
     }
 }
-
+let sync = false;
 const syncData = (req, res) => {
-    const token = getToken(req)
-    if(!token){
-        res.status(401).json('Not autorized')
+    if (sync) {
+        return res.status(429).send({ message: "Another sync is in progress" });
     }
-    const result = toDoService.syncData(req.body,token)
-    if (result) {
-        console.log("returning", 201)
-        res.status(201)
-    } else {
-        res.status(500)
+    sync = true;
+    try {
+        const token = getToken(req)
+        if (!token) {
+            res.status(401).json('Not autorized')
+        }
+        const result = toDoService.syncData(req.body, token)
+        if (result) {
+            console.log("returning", 201)
+            res.status(201)
+        } else {
+            res.status(500)
+        }
     }
+     catch (err) {
+        console.log(err)
+    }finally {
+        sync = false;
+      }
+
 
 }
 
